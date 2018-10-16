bc6cb6b28c8206852075073324d6557dfa15b24e
Silvie Chlupova <sisi.chlupova@gmail.com>
2018-10-16 12:56:32 +0200

    Added line with command_executor for remote browser

--- robottelo/config/base.py
+++ robottelo/config/base.py
@@ -1302,6 +1302,7 @@ class Settings(object):
                 'saucelabs_user': self.saucelabs_user,
                 'screenshots_path': self.screenshots_path,
                 'webdriver': self.webdriver,
+                'command_executor': self.command_executor,
                 'webdriver_binary': self.webdriver_binary,
             },
             'webdriver_desired_capabilities': (
